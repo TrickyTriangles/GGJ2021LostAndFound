e92205890f875b8b30df3bcbfd596cb4b4a40113
TrickyTriangles <47727993+TrickyTriangles@users.noreply.github.com>
2021-01-29 22:20:19 -0500

    First code commit

---
+++ GGJ2021 Lost and Found/Assets/Scripts/Player.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private enum PlayerState
+    {
+        STAND,
+        WALK,
+        DRAG
+    }
+
+    private PlayerState state;
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Transform grab_indicator;
+    [SerializeField] private float walk_speed;
+    [SerializeField] private float drag_speed;
+
+    private void Awake()
+    {
+        if (grab_indicator != null) { grab_indicator.gameObject.SetActive(false); }
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 direction;
+        HandleInput(out direction);
+
+        transform.Translate(direction * walk_speed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Stealable"))
+        {
+            grab_indicator.gameObject.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Stealable"))
+        {
+            grab_indicator.gameObject.SetActive(false);
+        }
+    }
+
+    private void HandleInput(out Vector3 direction)
+    {
+        direction = new Vector3();
+
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            direction.y = 1f;
+        }
+
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            direction.x = -1f;
+        }
+
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            direction.y = -1f;
+        }
+
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            direction.x = 1f;
+        }
+    }
+}
