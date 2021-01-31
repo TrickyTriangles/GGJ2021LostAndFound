291e221718c0c6eeaa8ebb8bd6c60c058cce1382
TrickyTriangles <47727993+TrickyTriangles@users.noreply.github.com>
2021-01-31 13:33:53 -0500

    MainMenuPanning

--- GGJ2021 Lost and Found/Assets/Scripts/CameraPan.cs
+++ GGJ2021 Lost and Found/Assets/Scripts/CameraPan.cs
@@ -4,13 +4,29 @@ using UnityEngine;
 
 public class CameraPan : MonoBehaviour
 {
+    [SerializeField] private Vector2 bounds;
+    [SerializeField] private float pan_speed;
+    private Vector3 direction;
+
+    private void Start()
+    {
+        direction = Vector3.up;
+    }
 
     // Update is called once per frame
     void Update()
     {
-        //Vector3 startPos = transform.position;
-        transform.Translate(Vector3.up * Time.deltaTime);
+        transform.Translate(direction * pan_speed * Time.deltaTime);
 
-       // transform.position = startPos;
+        if (transform.position.y > bounds.y)
+        {
+            transform.position = new Vector3(transform.position.x, bounds.y, transform.position.z);
+            direction *= -1f;
+        }
+        else if (transform.position.y < bounds.x)
+        {
+            transform.position = new Vector3(transform.position.x, bounds.x, transform.position.z);
+            direction *= -1f;
+        }
     }
 }
