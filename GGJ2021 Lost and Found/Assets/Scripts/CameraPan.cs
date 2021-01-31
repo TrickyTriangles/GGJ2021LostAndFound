ecd3433b4cbc92b140ca9a265e0a2c260903ff50
SemiMyth <SemiMyth@gmail.com>
2021-01-31 11:53:34 -0500

    new camera script

---
+++ GGJ2021 Lost and Found/Assets/Scripts/CameraPan.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraPan : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Vector3 direction ;
+        //direction.y = 1f;
+        //transform.Translate(direction * Time.deltaTime);
+    }
+}
